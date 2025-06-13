aebeab773c9ef02c0e404c392afd9d7773292eda
Domisnnet <dominique.goncalves61@gmail.com>
2025-06-13 17:30:37 -0300

    alterado

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
-// vue.config.j
+
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  transpileDependencies: true,
+
   publicPath: process.env.NODE_ENV === 'production'
     ? '/calculadora-em-Vue/'
     : '/'
